1cec8c7885278c6c224e1831ed8394b164b9be65
Chovhan <dema200043@gmail.com>
2020-02-16 16:30:28 +0200

    Labwork_4

---
+++ Lab_4/Labwork_4/src/main/java/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Messages messages = new Messages();
+        double userNumber = messages.inputNumber();
+        messages.printResult(calculateEquationOne(userNumber), "one");
+        messages.printResult(calculateEquationTwo(userNumber), "two");
+        messages.printResult(calculateEquationThree(userNumber), "three");
+        messages.printResult(calculateEquationFour(userNumber), "four");
+    }
+
+    public static double calculateEquationOne(double userNumber) {
+        return pow(userNumber, 1.752) + pow(userNumber, 3) * 2.031 - pow(userNumber, 2) * 3.874 + userNumber * 1.858;
+    }
+
+    public static double calculateEquationTwo(double userNumber) {
+        return pow(userNumber, 3) * 2.169 - pow(userNumber, 2) * 2.185 + userNumber * 5.896;
+    }
+
+    public static double calculateEquationThree(double userNumber) {
+        return pow(userNumber, 2) * 1.975 + userNumber * 3.065;
+    }
+
+    public static double calculateEquationFour(double userNumber) {
+        return userNumber * 4.15;
+    }
+
+    public static double pow(double firstNumber, double secondNumber){
+        return Math.pow(firstNumber, secondNumber);
+    }
+
+}
---
+++ Lab_4/Labwork_4/src/main/java/Messages.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Messages {
+
+    public double inputNumber(){
+        double userNumber;
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            try {
+                printInputText();
+                userNumber = scanner.nextDouble();
+                if (checkUserNumber(userNumber)){
+                    return userNumber;
+                }
+            } catch (Exception e){
+                printInvalidTypeExceptionText();
+            }
+        }
+    }
+
+    public boolean checkUserNumber(double userNumber) {
+        boolean check = true;
+        if (userNumber < 2.933 || userNumber > 101.90) {
+            printInvalidRangeExceptionText();
+            check = false;
+        }
+        return check;
+    }
+
+    public void printInvalidTypeExceptionText(){
+        System.out.println("Invalid data");
+    }
+
+    public void printInputText(){
+        System.out.println("Enter x between 2.934 and 101.89: ");
+    }
+
+    public void printInvalidRangeExceptionText(){
+        System.out.println("You have some errors with typing your number, enter a number between 2.934 and 101.89!");
+    }
+
+    public void printResult(double userNumber, String formulaNumber){
+        System.out.println("Answer with formula " + formulaNumber + " " + userNumber);
+    }
+
+}
